98034ded21adc54b5f2186aa9a70931feb8f9f2f
LateNightProgramming <124677881+LateNightProgramming@users.noreply.github.com>
2023-07-26 18:02:45 +1000

    Create meanCalculator.js

---
+++ meanCalculator.js
@@ -0,0 +1,43 @@
+function mathFunc(data) {
+    let mean = 0;
+    for (let i = 0; i < data.length; i++) {
+        mean+=data[i];
+    }
+    return mean/data.length;
+}
+
+function setup() {
+    let data = [];
+    let inp;
+    while (true) {
+        inp = prompt("enter a value ").toLowerCase();
+        if (inp === "end") {
+            console.log("selection finished");
+            break;
+        } else {
+            if (isNaN(Number(inp))) {
+                console.log("invalid response"); //alternative would be to throw a new exception if number(inp) = NaN
+            } else {
+                data.push(Number(inp));
+            }
+        }
+    }
+    console.log("mean = " + String(mathFunc(data)));
+    while (inp !== "yes" && inp !== "no") {
+        inp = prompt("do you wish to restart? ").toLowerCase();
+        switch (inp) {
+            case "yes":
+                console.log("restarting...");
+                setup();
+                break;
+            case "no":
+                console.log("finished...");
+                throw new Error();
+                break;
+            default:
+                console.log("invalid response")
+        }
+    }
+}
+console.log("welcome!\ntype end when you wish to end the data selection process\nbtw setup throws an error to end program when user desires");
+setup();
